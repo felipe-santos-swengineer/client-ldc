52324ae869045b4ea0f170a33b8f551addfdd69b
Felipe Santos <felipesantosswengineer@gmail.com>
2021-10-01 10:42:30 -0300

    with heroku

--- src/portas.js
+++ src/portas.js
@@ -1,6 +1,6 @@
 export default function portas(){
-    //var serverHost = "http://localhost:5000";
     var serverHost = "https://server-ldc.herokuapp.com";
+    var clientHost = "https://client-ldc.herokuapp.com";
 
     return {serverHost, clientHost};
 }
