98beda4899d7c06df4db7c52ef49e0ff1a97a31a
Felipe Santos <felipesantosswengineer@gmail.com>
2021-10-02 12:42:01 -0300

    with status_entrega

--- src/pages/v_aluno/view_inicial/Home.js
+++ src/pages/v_aluno/view_inicial/Home.js
@@ -1,11 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { makeStyles } from '@material-ui/core/styles';
 import NavBar from "../NavBar";
-import ImageList from '@material-ui/core/ImageList';
-import ImageListItem from '@material-ui/core/ImageListItem';
-import ImageListItemBar from '@material-ui/core/ImageListItemBar';
-import image from './images/pergamum.jpg';
-import image1 from "./images/flexibilizacao.jpg";
 import Paper from '@material-ui/core/Paper';
 import { Chart } from "react-google-charts";
 import Portas from "../../../portas";
@@ -17,45 +11,9 @@ import StoreContext from '../../../components/Store/Context';
 import { useContext } from 'react';
 
 
-const useStyles = makeStyles((theme) => ({
-    root: {
-        display: 'flex',
-        flexWrap: 'wrap',
-        justifyContent: 'center',
-        overflow: 'hidden',
-        backgroundColor: theme.palette.background.paper,
-        marginTop: "20px",
-        marginLeft: "20px",
-        marginRight: "20px",
-    },
-    paper: {
-        marginTop: "20px",
-        marginLeft: "20px",
-        marginRight: "20px",
-        marginBottom: "20px",
-        paddingTop: "20px",
-        paddingLeft: "20px",
-        paddingRight: "20px",
-        paddingBottom: "20px",
-        backgroundColor: theme.palette.background.paper,
-    },
-    imageListHorizontal: {
-        flexWrap: 'nowrap',
-        // Promote the list into his own layer on Chrome. This cost memory but helps keeping high FPS.
-        transform: 'translateZ(0)',
-    },
-    title: {
-        color: theme.palette.primary.light,
-    },
-    titleBar: {
-        background:
-            'linear-gradient(to top, rgba(0,0,0,0.7) 0%, rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)',
-    },
-}));
-
 export default function Home() {
-    const classes = useStyles();
     const { token } = useContext(StoreContext);
+    const [status, setStatus] = useState(false);
     const [options, setOptions] = useState();
     const [optionsBar, setOptionsBar] = useState();
     const [data, setData] = useState([]);
@@ -63,39 +21,7 @@ export default function Home() {
 
     );
     const [optionsTotal, setOptionsTotal] = useState();
-    const [dataBar, setDataBar] = useState([])
-    const itemData = [
-        {
-            img: image,
-            title: 'Image1',
-            author: 'author',
-        },
-        {
-            img: image1,
-            title: 'Image2',
-            author: 'author',
-        },
-        {
-            img: image,
-            title: 'Image3',
-            author: 'author',
-        },
-        {
-            img: image1,
-            title: 'Image4',
-            author: 'author',
-        },
-        {
-            img: image,
-            title: 'Image5',
-            author: 'author',
-        },
-        {
-            img: image1,
-            title: 'Image6',
-            author: 'author',
-        },
-    ];
+    const [dataBar, setDataBar] = useState([]);
 
     const somarAtividades = (resJSON) => {
         //somar atividades
@@ -233,32 +159,45 @@ export default function Home() {
         }
     }
 
+    const getStatus = async () => {
+        try {
+            const response = await fetch(Portas().serverHost + "/alunos/byToken/" + token,
+                {
+                    method: "GET",
+                }
+            );
+            var resJSON = await response.json();
+            if(resJSON.status_entrega === "true"){
+                setStatus(true);
+            }
+            else{
+                setStatus(false);
+            }
+            
+
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
     useEffect(() => {
         getAtividades();
+        getStatus();
     }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
 
     return (
         <div>
             <NavBar></NavBar>
-            <div className={classes.root}>
-                <ImageList className={classes.imageListHorizontal} cols={2.5}>
-                    {itemData.map((item) => (
-                        <ImageListItem key={item.img}>
-                            <img src={item.img} alt={item.title} />
-                            <ImageListItemBar
-                                title={item.title}
-                                classes={{
-                                    root: classes.titleBar,
-                                    title: classes.title,
-                                }}
-                            />
-                        </ImageListItem>
-                    ))}
-                </ImageList>
-            </div>
             <Paper elevation={12} style={{ textAlign: "center", marginTop: "20px", marginLeft: "20px", marginRight: "20px", marginBottom: "20px" }}>
-                <h1>Resumo das suas Atividades</h1>
+                <h1>Status das atividades:</h1>
+                {status? <h2 style={{ color: "green" }}>Atividades Aceitas</h2> : <h2 style={{ color: "red" }}>Não concluidas</h2> }
+                <br></br>
+            </Paper>
+            <Paper elevation={12} style={{ textAlign: "center", marginTop: "20px", marginLeft: "20px", marginRight: "20px", marginBottom: "20px" }}>
+
+                <h1>Resumo das suas Atividades (Baseado em atividades cadastradas) </h1>
+
                 <div style={{ display: "flex", overflowY: "scroll", marginTop: "20px", marginLeft: "20px", marginRight: "20px", marginBottom: "20px" }}>
                     <Chart
                         width={'500px'}
--- src/pages/v_avaliador/view_inicial/Home.js
+++ src/pages/v_avaliador/view_inicial/Home.js
@@ -1,10 +1,14 @@
 import NavBar from "../NavBar";
 import Copyright from "../../copyright/Copyright";
+import Paper from '@material-ui/core/Paper';
 
-export default function Home(){
+export default function Home() {
     return (
         <div>
             <NavBar></NavBar>
+            <Paper elevation={12} style={{display: 'flex', flexWrap: 'wrap', justifyContent: 'center',overflow: 'hidden',}}>
+                <h1>Welcome Avaliador</h1>
+            </Paper>
             <Copyright></Copyright>
         </div>
     )
