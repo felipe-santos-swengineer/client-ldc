690aa5265b35c1c5b10dfb8b71064f7dcb8b784e
Felipe Santos <felipesantosswengineer@gmail.com>
2021-12-06 19:20:30 -0300

    100%

--- src/components/copyright/Copyright.js
+++ src/components/copyright/Copyright.js
@@ -1,15 +1,13 @@
 import Box from '@material-ui/core/Box';
 import Typography from '@material-ui/core/Typography';
-import Link from '@material-ui/core/Link';
 
 export default function Copyright() {
     return (
         <Box pt={4}>
             <Typography variant="body2" color="textSecondary" align="center">
                 {'Direitos reservados ©'}
-                <Link color="primary" href="https://team-pldc.herokuapp.com/" target="_blank">
-                    Projeto Linha de Chegada
-                </Link>{' '}
+                {'Projeto Linha de Chegada '}
+                
                 {new Date().getFullYear()}
                 {'.'}
             </Typography>
